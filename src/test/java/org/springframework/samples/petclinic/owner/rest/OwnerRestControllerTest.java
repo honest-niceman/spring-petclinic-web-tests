57073ba1a76bb78469d2bf8cf27e4bf202e7057c
Georgii Vlasov <honest.niceman@gmail.com>
2024-10-31 15:09:20 +0400

    2. Set up properties and compose

--- src/test/java/org/springframework/samples/petclinic/owner/rest/OwnerRestControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/rest/OwnerRestControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.samples.petclinic.owner.OwnerRepository;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -18,6 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * Test class for the {@link OwnerRestController}
  */
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 @AutoConfigureMockMvc
 public class OwnerRestControllerTest {
 
